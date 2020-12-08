fd7d973f3e46858905f104a7e047898c8ed65292
passakorn2663 <passakornbuakeaw@gmail.com>
2020-12-09 00:22:42 +0700

    First send HW

---
+++ lib/myapp.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:homework/posts_page.dart';
+
+class MyApp extends MaterialApp {
+  MyApp() : super(home: PostsPage());
+}
---
+++ lib/posts_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PostsList extends StatefulWidget {
+  @override
+  _PostsListState createState() => _PostsListState();
+}
+
+class _PostslistState extends State<PostsList> {
+  @override
+  void initState() {}
+
+  @override
+  Widget build(BuildContext context) {}
+}
